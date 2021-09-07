c026a2d828a68ed7edc3ecd23457ce50dabf9545
jinseonghee <wlstjdgml0610@gmail.com>
2021-09-07 14:33:11 +0900

    rest-template을 이용하여 get 방식으로 통신

--- server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -1,7 +1,9 @@
 package com.example.server.controller;
 
+import com.example.server.dto.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ServerApiController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "hello server";
+    public User hello(@RequestParam String name, @RequestParam int age) { //api Controller에서 요청이 들어오면 여기서 response를 내려줌
+                                                                  //Client의 Queryparameter를 넣어준걸 여기서 @RequestParam으로 받아줌.
+
+        User user = new User();
+        //user.setName("steve");
+        user.setName(name); //Requestparameter를 받을 경우 echo로 동작
+        user.setAge(age);
+        //user.setAge(10);
+        return user; //그냥 Object로 return 시킴
+        //return "hello server";
     }
 }
