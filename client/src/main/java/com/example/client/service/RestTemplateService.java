02c221fb4a56dd1416af688dadfc398e189c948e
jinseonghee <wlstjdgml0610@gmail.com>
2021-09-13 23:27:12 +0900

    edit comment

--- client/src/main/java/com/example/client/service/RestTemplateService.java
+++ client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -5,6 +5,7 @@ import com.example.client.dto.UserRequest;
 import com.example.client.dto.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -32,7 +33,7 @@ public class RestTemplateService {
         System.out.println(uri.toString());// 위의 주소가 잘 만들어 졌는지 확인
 
         RestTemplate restTemplate = new RestTemplate(); //RestTemplate을 통해서 통신을 한다.
-        //String result = restTemplate.getForObject(uri, String.class); //uri에 요청을 시키고 문자열로 받는다(response로 받을 형태(String) 지정).
+        //String result = restTemplate.getForObject(uri  , String.class); //uri에 요청을 시키고 문자열로 받는다(response로 받을 형태(String) 지정).
         //getForObject가 실행되는 순간이 client에서 http 서버로 붙는 순간임.
         //getForObject는 http의 메서드이다.
 
