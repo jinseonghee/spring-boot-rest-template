5a333ef727a12d48e8a61d458945a79632da879e
jinseonghee <wlstjdgml0610@gmail.com>
2021-10-12 23:09:08 +0900

    Naver 지역 검색 API 연동해보기

--- server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -4,12 +4,58 @@ import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    //https://openapi.naver.com/v1/search/local.json
+    // ?query=%EC%A3%BC%EC%8B%9D
+    // &display=10
+    // &start=1
+    // &sort=random
+
+    @GetMapping("/naver")
+    public String naver() {
+
+        String query = "갈비집";
+        //String encode = Base64.getEncoder().encodeToString(query.getBytes(StandardCharsets.UTF_8));
+
+
+        URI uri = UriComponentsBuilder//주소 만드는 uriBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("v1/search/local.json")
+                .queryParam("query", "갈비집")
+                .queryParam("display", "10")
+                .queryParam("start", "1")
+                .queryParam("sort", "random")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity //RequestEntity는 header를 사용하기 위해 사용 , Get으로 넣을 거니까 들어갈게 없어서 void를 해줌
+                .get(uri) //get방식에 uri를 넣을 것이라는 뜻
+                .header("X-Naver-Client-Id", "9xDkgqPP5wx72xCn_og0")
+                .header("X-Naver-Client-Secret", " kuj400PhD7")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
+    }
 
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age) { //api Controller에서 요청이 들어오면 여기서 response를 내려줌
